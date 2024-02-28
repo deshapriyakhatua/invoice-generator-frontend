1ac6eb36f39d9a7269b1e6e418ddd57bf4574bfc
deshapriya <deshapriyakhatua@gmail.com>
2024-02-28 11:48:22 +0530

    first commit

---
+++ user/sidebar/sidebar.js
@@ -0,0 +1,10 @@
+
+/* =============== Menu toggle=============== */
+
+const showSideBar = function (){
+    document.querySelector(".sidebar").classList.remove("hide_sidebar");
+}
+
+const hideSideBar = function (){
+    document.querySelector(".sidebar").classList.add("hide_sidebar");
+}
