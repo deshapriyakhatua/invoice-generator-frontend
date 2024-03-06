0403afacc79adbda43e7209a93ab3ae4d2211d3c
deshapriya <deshapriyakhatua@gmail.com>
2024-03-06 21:35:49 +0530

    invoice 2 added

--- user/invoice2/invoice.js
+++ user/invoice2/invoice.js
@@ -116,6 +116,30 @@ removeFileButton.addEventListener("click", () => {
 });
 
 
+/*-------------- Auto Resize textarea --------------- */
+
+function autoResizeTextArea(){
+    const txHeight = 26;
+    const tx = document.getElementsByTagName("textarea");
+
+    for (let i = 0; i < tx.length; i++) {
+    if (tx[i].value == '') {
+        tx[i].setAttribute("style", "height:" + txHeight + "px;overflow-y:hidden;");
+    } else {
+        tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    }
+    tx[i].addEventListener("input", OnInput, false);
+    }
+
+    function OnInput(e) {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + "px";
+    }
+}
+
+autoResizeTextArea();
+
+
  /*------------- Open & Close upload popup ----------- */
 
 document.querySelector(".invoice_logo_holder").addEventListener("click", (event) => {
@@ -134,7 +158,7 @@ document.querySelector(".invoice_section3 .add_new_row td div.add_line").addEven
     itemRow.className = "item_row";
     itemRow.innerHTML = `
                         <td class="first">
-                            <input type="text" class="item_name" placeholder="Beats Studio Over-Ear Headphones &#128393;">
+                            <textarea name="item_name" class="item_name" id="" cols="30" rows="1" placeholder="Enter Product Title Here   🖊" spellcheck="false"></textarea>
                         </td>
                         <td class="second">
                             <input type="number" class="unit_price" value="100">
@@ -166,23 +190,78 @@ document.querySelector(".invoice_section3 .add_new_row td div.add_line").addEven
     let afterItemRow = document.querySelector(".invoice_section3 .add_new_row");
     parentNode.insertBefore(itemRow,afterItemRow);
     parentNode.insertBefore(rowSeparator,afterItemRow);
+    autoResizeTextArea();
     calculateTotalPrice();
 });
 
 
-    function removeRowItem(event){
-        let parent = event.target.parentNode.parentNode.parentNode;
-        let row = event.target.parentNode.parentNode;
-        let rowSeparator = event.target.parentNode.parentNode.nextElementSibling
-        console.log(rowSeparator)
-        parent.removeChild(row)
-        parent.removeChild(rowSeparator)
-        console.log(row)
-       //allRows[allRows.length-5].remove();
-       //allRows[allRows.length-6].remove();
-        
-        calculateTotalPrice();
-    };
+function removeRowItem(event){
+    let parent = event.target.parentNode.parentNode.parentNode;
+    let row = event.target.parentNode.parentNode;
+    let rowSeparator = event.target.parentNode.parentNode.nextElementSibling
+    console.log(rowSeparator)
+    parent.removeChild(row)
+    parent.removeChild(rowSeparator);
+            
+    calculateTotalPrice();
+};
+
+/*------------------- Add and Remove Business details ---------------- */
+
+document.querySelector(".invoice_section2 .business_details div.add_line").addEventListener("click", (event)=>{
+    let itemRow = document.createElement("div");
+    itemRow.innerHTML = `
+        <input type="text" value="" placeholder="Enter Custom Value">
+        <span class="material-symbols-rounded remove-line-icon" onclick="removeBusinessDetailsRow(event)">cancel</span>
+    `;
+
+    let parentNode = document.querySelector(".invoice_section2 .business_details");
+    let afterItemRow = document.querySelector(".invoice_section2 .business_details div.add_line");
+    parentNode.insertBefore(itemRow,afterItemRow);
+});
+
+function removeBusinessDetailsRow(event){
+    let parent = event.target.parentNode.parentNode;
+    let row = event.target.parentNode;
+    parent.removeChild(row)
+};
+
+
+/*------------------- Add and Remove Billing & shipping details ---------------- */
+
+document.querySelector(".invoice_section4 .billig div.add_line").addEventListener("click", ()=>{
+     addNewRowInput(".invoice_section4 .billig", ".invoice_section4 .billig div.add_line"); 
+});
+
+document.querySelector(".invoice_section4 .shipping div.add_line").addEventListener("click", ()=>{
+    addNewRowInput(".invoice_section4 .shipping", ".invoice_section4 .shipping div.add_line"); 
+});
+
+document.querySelector(".invoice_section4 .payment_method div.add_line").addEventListener("click", ()=>{
+    addNewRowInput(".invoice_section4 .payment_method", ".invoice_section4 .payment_method div.add_line"); 
+});
+
+document.querySelector(".invoice_section4 .shipping_method div.add_line").addEventListener("click", ()=>{
+    addNewRowInput(".invoice_section4 .shipping_method", ".invoice_section4 .shipping_method div.add_line"); 
+});
+
+function addNewRowInput(parent, afterItem){
+    let itemRow = document.createElement("div");
+    itemRow.innerHTML = `
+    <input type="text" placeholder="Enter Custom Value">
+    <span class="material-symbols-rounded remove-line-icon" onclick="removeBillingDetailsRow(event)">cancel</span>
+    `;
+
+    let parentNode = document.querySelector(parent);
+    let afterItemRow = document.querySelector(afterItem);
+    parentNode.insertBefore(itemRow,afterItemRow);
+};
+
+function removeBillingDetailsRow(event){
+    let parent = event.target.parentNode.parentNode;
+    let row = event.target.parentNode;
+    parent.removeChild(row);
+};
 
 
 /*------------------- Calculate invoice price ------------------ */
