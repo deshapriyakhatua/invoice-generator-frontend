a6a72087b66b4057474fda393b4d4f72176d3f93
deshapriya <deshapriyakhatua@gmail.com>
2024-03-09 21:21:48 +0530

    page updated

--- user/invoice/invoice.js
+++ user/invoice/invoice.js
@@ -1,8 +1,11 @@
+window.addEventListener("load", ()=>{
+
 var isAdvancedUpload = function () {
     var div = document.createElement('div');
     return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
 }();
 
+let iframe = document.querySelector(".invoice_iframe");
 let draggableFileArea = document.querySelector(".drag-file-area");
 let browseFileText = document.querySelector(".browse-files");
 let uploadIcon = document.querySelector(".upload-icon");
@@ -49,7 +52,7 @@ uploadButton.addEventListener("click", () => {
                     uploadButton.innerHTML = `<span class="material-symbols-rounded upload-button-icon"> check_circle </span> Uploaded`;
                     let reader = new FileReader();
                     reader.onload = function (e) {
-                        document.querySelector(".invoice_logo").src = e.target.result;
+                        iframe.contentWindow.document.querySelector(".invoice_logo").src = e.target.result;
                     }
                     reader.readAsDataURL(fileInput.files[0]);
                     document.querySelector(".image_upload").style.display = "none";
@@ -116,9 +119,33 @@ removeFileButton.addEventListener("click", () => {
 });
 
 
+/*-------------- Auto Resize textarea --------------- */
+
+function autoResizeTextArea(){
+    const txHeight = 26;
+    const tx = document.getElementsByTagName("textarea");
+
+    for (let i = 0; i < tx.length; i++) {
+    if (tx[i].value == '') {
+        tx[i].setAttribute("style", "height:" + txHeight + "px;overflow-y:hidden;");
+    } else {
+        tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    }
+    tx[i].addEventListener("input", OnInput, false);
+    }
+
+    function OnInput(e) {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + "px";
+    }
+}
+
+autoResizeTextArea();
+
+
  /*------------- Open & Close upload popup ----------- */
 
-document.querySelector(".invoice_logo_holder").addEventListener("click", (event) => {
+ iframe.contentWindow.document.querySelector(".invoice_logo_holder").addEventListener("click", (event) => {
     document.querySelector(".image_upload").style.display = "flex";
 });
 
@@ -127,87 +154,12 @@ document.querySelector(".close_drag_drop").addEventListener("click", (event) => 
 });
 
 
-/* ------------ Add & remove item row ---------- */
 
-document.querySelector(".item_add_remove .add_item").addEventListener("click", (event)=>{
-    //if(document.querySelectorAll(".invoice_section3 tbody tr").length >= 25){ return; }
-    let itemRow = document.createElement("tr");
-    itemRow.className = "item_row";
-    itemRow.innerHTML = `
-                        <td class="first">
-                            <input type="text" class="item_name" placeholder="Beats Studio Over-Ear Headphones &#128393;">
-                        </td>
-                        <td class="second">
-                            <input type="number" class="unit_price" value="100">
-                        </td>
-                        <td class="third">
-                            <input type="number" class="cgst" value="89">
-                        </td>
-                        <td class="fourth">
-                            <input type="number" class="sgst" value="89">
-                        </td>
-                        <td class="fifth">
-                            <input type="number" class="quantity" value="1">
-                        </td>
-                        <td class="sixth">
-                            <input type="number" class="price" value="859" disabled>
-                        </td>
-    `;
-
-    let rowSeparator = document.createElement("tr");
-    rowSeparator.className = "row_separator";
-    rowSeparator.innerHTML = `
-                        <td height="1" colspan="6"></td>
-    `;
-
-    let parentNode = document.querySelector(".invoice_section3 tbody");
-    let afterItemRow = document.querySelector(".after_item_row");
-    parentNode.insertBefore(itemRow,afterItemRow);
-    parentNode.insertBefore(rowSeparator,afterItemRow);
-    calculateTotalPrice();
-});
-
-document.querySelector(".item_add_remove .remove_item").addEventListener("click", (event)=>{
-     let allRows = document.querySelectorAll(".invoice_section3 tbody tr");
-     if(allRows.length > 8){
-        allRows[allRows.length-5].remove();
-        allRows[allRows.length-6].remove();
-     }
-     calculateTotalPrice();
-});
-
-
-/*------------------- Calculate invoice price ------------------ */
-
-function calculateTotalPrice(){
-
-    let subTotal = 0;
-
-    document.querySelectorAll(".item_row").forEach((elem)=>{
-        let unitPrice = elem.querySelector(".unit_price").value * 1 + elem.querySelector(".cgst").value * 1 + elem.querySelector(".sgst").value * 1;
-        let quantity = elem.querySelector(".quantity").value * 1;
-
-        let price = unitPrice * quantity;
-        elem.querySelector(".price").value = price;
-        subTotal += price;
-    });
-
-    document.querySelector(".subtotal_row .second input").value = subTotal;
-
-    subTotal += document.querySelector(".shipping_row .second input").value*1;
-
-    document.querySelector(".grand_total_row .second input").value = subTotal;
-}
-
-calculateTotalPrice();
-
-window.addEventListener('input', function (evt) {
-    calculateTotalPrice();
-});
 
 /*------------- print invoice -----------*/
 
 document.querySelector(".print_button").addEventListener("click", () => {
-    window.print();
+    document.querySelector(".invoice_iframe").contentWindow.print();
 });
 
+});
